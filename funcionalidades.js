41e426a2f813aaa3ee4345630bbb9e1a6f6c650d
florencia <maryflopy85@hotmail.com.ar>
2024-05-22 09:46:32 -0300

    modifique el documento de js

--- funcionalidades.js
+++ funcionalidades.js
@@ -2,94 +2,23 @@
 var topping = prompt("Ingrese topping");
 
 var topping;
-var precio;
-var preciofinal;
-var helado;
+var Preciofinal = 10000
 
-if(topping = "oreo")
-{
-    console.log("oreo");
+if(topping == "oreo"){
+     console.log(Preciofinal + 10);
 } else if(topping="kitkat"){
-    console.log("kitkat");
+    console.log(Preciofinal + 15);
 } else if(topping="kinder"){
-    console.log("kinder");
+    console.log(Preciofinal + 25);
 }
 
-switch (topping) {
-    case "oreo":
-        console.log("El precio de oreo es $10")
-        break;
-    case "kitkat":
-        console.log("El precio de kitkat es $15")
-        break;
-    case "kinder":
-        console.log("El precio de kinder es $25")
-        break;
-    default:
-        console.log("No tenemos ese topping")
-}
-console.log("Fin switch case");
-
-var tamaño = prompt("Ingrese tamaño");
-
-var topping;
-var precio;
-var preciofinal;
-var helado;
-var tamaño;
-
-
-
-if(tamaño = "1kilo")
-{
-    console.log("1kilo");
-} else if(tamaño = "1/2kilo"){
-    console.log("1/2kilo");
-} else if(tamaño = "1/4kilo"){
-    console.log("1/4kilo");
-} else if(tamaño = "1bocha"){
-    console.log("1bocha");
-} else if(tamaño = "2bochas"){
-    console.log("2bochas");
-} else if(tamaño = "vasito"){
-    console.log("vasito");
-}
-
-switch (tamaño) {
-    case "1kilo":
-        console.log("Su precio es $6000")
-    break;
-    case "1/2kilo":
-        console.log("$3000")
-    break;
-    case "1/4kilo":
-        console.log("$2000")
-    break;
-    case "1bocha":
-        console.log("$1000")
-    break;
-    case "2bochas":
-         console.log("$2000")
-    break;
-    case "vasito":
-        console.log("$2000")
-    break;
-    default: 
-        console.log("No tenemos ese tamaño")
-        break;
-}
-console.log("Fin switch case");
 
 
 var menu= prompt("Ingrese menu");
 var menu;
-var carne;
-var pescado;
-var verdura;
 
 
-if(menu = "carne")
-{
+if(menu == "carne"){
     console.log("carne");
 } else if(menu = "pescado"){
     console.log("pescado");
@@ -114,21 +43,20 @@ switch (menu) {
 console.log("Fin switch case");
 
 var array = [1,2,3,4,5,6];
+var arraymostrar = [];
 for (let index = 0 ; index < array.length ; index++){
-    console.log(array[1]);
+    arraymostrar.push(array[index]);
     
 }
 
-for (let posicion = 0 ; posicion < array.length ; posicion++){
-    console.log(array[posicion]);
+for (let index = 0 ; index< arraymostrar.length ; index++){
+    console.log(array[index]);
 }
 
-var numero = false;
+var numero = 0;
 
-while (numero == false ) {
-    console.log(numero<11);
-    console.log("Numero es mas chico que 11");
-
-    numero = true;
+while (numero<11){
+    console.log(numero + " es menor que 11");
+     numero++
 }
 console.log("Fin while");
